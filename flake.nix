27ec0d534b7cc3ef4080fc04602c193ad0a48b97
Alan Roe <alanroe92@gmail.com>
2023-10-29 20:32:35 +0000

    fix: gtk schemas for desktop color-chooser display

--- flake.nix
+++ flake.nix
@@ -19,8 +19,12 @@
       unstable = import nixpkgs-unstable {
           inherit system;
       };
+      desktop-schemas = with pkgs; "${gtk3}/share/gsettings-schemas/${gtk3.name}";
     in {
       devShells.${system}.default = pkgs.mkShell {
+        shellHook = ''
+          export XDG_DATA_DIRS=${desktop-schemas}:$XDG_DATA_DIRS
+        '';
         buildInputs = with pkgs; [
           (with fenix.packages.${system}; with stable; combine [
             cargo rustc rust-src rustfmt clippy rust-analyzer targets.wasm32-unknown-unknown.stable.rust-std
